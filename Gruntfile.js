814cf32c7f59eb7396d28d895f4bcba4477abecd
Andrew Pennebaker <andrew.pennebaker@networkedinsights.com>
2015-02-25 13:44:17 -0600

    jslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,12 @@
+"use strict";
+
 module.exports = function(grunt) {
   grunt.initConfig({
 		exec: {
 			test: "npm test",
 
       jshint: "node_modules/jshint/bin/jshint .",
+      jslint: "find . -type d -name node_modules -prune -o -type d -name bower_components -prune -o -type f -name '*[-.]min.js' -prune -o -type f -name '*.js' -exec node_modules/jslint/bin/jslint.js {} \\;",
       aspelllint: "bundle exec aspelllint",
       lili: "bundle exec lili ."
 		}
@@ -16,10 +19,13 @@ module.exports = function(grunt) {
 
   grunt.registerTask("lint", [
     "exec:jshint",
+    // Go to bed, Crockford
+    // "exec:jslint",
     "exec:lili"
   ]);
 
   grunt.registerTask("jshint", ["exec:jshint"]);
+  grunt.registerTask("jslint", ["exec:jslint"]);
   grunt.registerTask("aspelllint", ["exec:aspelllint"]);
   grunt.registerTask("lili", ["exec:lili"]);
 };
