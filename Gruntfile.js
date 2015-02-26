47eac467acb9befb3dfe62ed0a7e84467def4299
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2015-02-25 22:23:31 -0600

    eslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,7 @@ module.exports = function(grunt) {
 
       jshint: "node_modules/jshint/bin/jshint .",
       jslint: "find . -type d -name node_modules -prune -o -type d -name bower_components -prune -o -type f -name '*[-.]min.js' -prune -o -type f -name '*.js' -exec node_modules/jslint/bin/jslint.js {} \\;",
+      eslint: "node_modules/eslint/bin/eslint.js .",
       aspelllint: "bundle exec aspelllint",
       lili: "bundle exec lili ."
 		}
@@ -21,11 +22,13 @@ module.exports = function(grunt) {
     "exec:jshint",
     // Go to bed, Crockford
     // "exec:jslint",
+    "exec:eslint",
     "exec:lili"
   ]);
 
   grunt.registerTask("jshint", ["exec:jshint"]);
   grunt.registerTask("jslint", ["exec:jslint"]);
+  grunt.registerTask("eslint", ["exec:eslint"]);
   grunt.registerTask("aspelllint", ["exec:aspelllint"]);
   grunt.registerTask("lili", ["exec:lili"]);
 };
