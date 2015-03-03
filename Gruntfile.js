914a5e797a240031fbe879b656e3970cf63c29c3
Andrew Pennebaker <andrew.pennebaker@networkedinsights.com>
2015-03-03 11:43:04 -0600

    editorconfig, eslint, jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,37 @@
-"use strict";
+'use strict';
 
 module.exports = function(grunt) {
   grunt.initConfig({
 		exec: {
-			test: "npm test",
+			test: 'npm test',
 
-      jshint: "node_modules/jshint/bin/jshint .",
-      jslint: "find . -type d -name node_modules -prune -o -type d -name bower_components -prune -o -type f -name '*[-.]min.js' -prune -o -type f -name '*.js' -exec node_modules/jslint/bin/jslint.js {} \\;",
-      eslint: "node_modules/eslint/bin/eslint.js .",
-      aspelllint: "bundle exec aspelllint",
-      lili: "bundle exec lili ."
+      jshint: 'node_modules/jshint/bin/jshint .',
+      jslint: 'find . -type d -name node_modules -prune -o -type d -name bower_components -prune -o -type f -name "*[-.]min.js" -prune -o -type f -name "*.js" -exec node_modules/jslint/bin/jslint.js {} \\;',
+      eslint: 'node_modules/eslint/bin/eslint.js .',
+      aspelllint: 'bundle exec aspelllint',
+      lili: 'bundle exec lili .',
+      editorconfig: 'find . -type f -name Thumbs.db -prune -o -type f -name .DS_Store -prune -o -type d -name .git -prune -o -type d -name .svn -prune -o -type d -name tmp -prune -o -type d -name bin -prune -o -type d -name target -prune -o -name "*.app*" -prune -o -type d -name node_modules -prune -o -type d -name bower_components -prune -o -type f -name "*[-.]min.js" -prune -o -type d -name "*.dSYM" -prune -o -type f -name "*.scpt" -prune -o -type d -name "*.xcodeproj" -prune -o -type d -name .vagrant -prune -o -type f -name .exe -prune -o -type f -name "*.o" -prune -o -type f -name "*.pyc" -prune -o -type f -name "*.hi" -prune -o -type f -name "*.beam" -prune -o -type f -name "*.png" -prune -o -type f -name "*.gif" -prune -o -type f -name "*.jp*g" -prune -o -type f -name "*.ico" -prune -o -type f -name "*.ttf" -prune -o -type f -name "*.zip" -prune -o -type f -name "*.jar" -prune -o -type f -name "*.dot" -prune -o -type f -name "*.pdf" -prune -o -type f -name "*.wav" -prune -o -type f -name "*.mp[34]" -prune -o -type f -name "*.svg" -prune -o -type f -name "*.flip" -prune -o -type f -name "*.class" -prune -o -type f -name "*.jad" -prune -o -type d -name .idea -prune -o -type f -name "*.iml" -prune -o -type f -name "*.log" -prune -o -type f -name "*" -exec node_modules/editorconfig-tools/bin/index.js check {} \\;'
 		}
   });
 
-  grunt.loadNpmTasks("grunt-exec");
+  grunt.loadNpmTasks('grunt-exec');
 
-  grunt.registerTask("default", ["exec:test"]);
-  grunt.registerTask("test", ["exec:test"]);
+  grunt.registerTask('default', ['exec:test']);
+  grunt.registerTask('test', ['exec:test']);
 
-  grunt.registerTask("lint", [
-    "exec:jshint",
+  grunt.registerTask('lint', [
+    'exec:jshint',
     // Go to bed, Crockford
-    // "exec:jslint",
-    "exec:eslint",
-    "exec:lili"
+    // 'exec:jslint',
+    'exec:eslint',
+    'exec:lili',
+    'exec:editorconfig'
   ]);
 
-  grunt.registerTask("jshint", ["exec:jshint"]);
-  grunt.registerTask("jslint", ["exec:jslint"]);
-  grunt.registerTask("eslint", ["exec:eslint"]);
-  grunt.registerTask("aspelllint", ["exec:aspelllint"]);
-  grunt.registerTask("lili", ["exec:lili"]);
+  grunt.registerTask('jshint', ['exec:jshint']);
+  grunt.registerTask('jslint', ['exec:jslint']);
+  grunt.registerTask('eslint', ['exec:eslint']);
+  grunt.registerTask('aspelllint', ['exec:aspelllint']);
+  grunt.registerTask('lili', ['exec:lili']);
+  grunt.registerTask('editorconfig', ['exec:editorconfig']);
 };
