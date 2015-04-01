1f44f3612b8643a3b92409954dc843bafaf7fc09
Andrew Pennebaker <andrew.pennebaker@networkedinsights.com>
2015-04-01 10:53:27 -0500

    jslint bin/ scripts

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
 			test: 'npm test',
 
       jshint: 'node_modules/jshint/bin/jshint .',
-      jslint: 'find . -type d -name node_modules -prune -o -type d -name bower_components -prune -o -type f -name "*[-.]min.js" -prune -o -type f -name "*.js" -exec node_modules/jslint/bin/jslint.js {} \\;',
+      jslint: 'find . -type d -name node_modules -prune -o -type d -name bower_components -prune -o -type f -name "*[-.]min.js" -prune -o -type f -name "*.bat" -prune -o -type f \\( -wholename "*/bin/*" -or -name "*.js" \\) -exec node_modules/jslint/bin/jslint.js {} \\;',
       eslint: 'node_modules/eslint/bin/eslint.js .',
       aspelllint: 'bundle exec aspelllint',
       lili: 'bundle exec lili .',
